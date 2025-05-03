0450296d27614bd4ef55db693484113376e8344e
RBLucky <nkosi.s.lucky@gmail.com>
2025-05-03 22:32:42 +0200

    set up express

--- server.js
+++ server.js
@@ -2,6 +2,23 @@
 
 // Import packages and modules
 import express from 'express';
+import dotenv from 'dotenv';
 
 // Initialize Express
 const app = express();
+
+// Configure environment variables
+dotenv.config();
+
+// File Serving
+app.use(express.static('client'));
+
+// Home page route
+app.get('/', requestAnimationFrame, res => {
+    res.sendFile(__dirname + '/client/index.html');
+})
+
+// Run the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  });
